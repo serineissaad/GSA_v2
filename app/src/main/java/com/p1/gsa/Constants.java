7580545fe3148430e27c6d55509238846dde99a2
MacBook Pro <syrineissaad2@gmail.com>
2022-03-30 19:19:26 +0100

    mines + lyna signup fixed

--- app/src/main/java/com/p1/gsa/Constants.java
+++ app/src/main/java/com/p1/gsa/Constants.java
@@ -3,6 +3,7 @@ package com.p1.gsa;
 public class Constants {
     private static final String ROOT_URL=" http://192.168.64.2/gsaphp/";
     public static final String URL_REGISTER=ROOT_URL+"signup.php";
+    public static final String URL_REGISTERASS=ROOT_URL+"signupass.php";
     public static final String URL_LOGIN=ROOT_URL+"login.php";
     public static final String URL_GETASS=ROOT_URL+"activateass.php";
     public static final String URL_DELETEASS=ROOT_URL+"deleteass.php";
